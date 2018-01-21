ca8e206f928b64bf1e4b1f3a7191607222b7add7
tahirkalim <thrkalim@gmail.com>
2018-01-21 05:03:11 +0100

    add validations to article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,13 +5,18 @@ class ArticlesController < ApplicationController
   def new
   	@article = Article.new
   end
-
+  
   def create
-	@article = Article.new(article_params) 
-	@article.save
-	flash[:success] = "Article has been created" 
-	redirect_to articles_path
+  @article = Article.new(article_params) 
+  if @article.save
+  flash[:success] = "Article has been created"
+  redirect_to articles_path 
+  else
+  flash[:danger] = "Article has not been created"
+  render :new
   end
+end
+
 
 private
 def article_params
